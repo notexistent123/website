daf389eb6506d1c41413aa37831895ce3a33d6d4
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2022-05-09 06:39:16 +0800

    got some stuff on the page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,22 @@
 <h1 class="title">Help Me Pls // Crazypersonalph // Alphons Joseph</h1>
+
+<script>
+	import { onMount } from 'svelte';
+	import AOS from 'aos';
+	
+	onMount(() => {
+		AOS.init()
+	})
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</svelte:head>
+<div data-aos="fade-up">
+	<p class="intro">Hello, I am Alphons. I am a student</p>
+</div>
+
+<div data-aos="slide-left">
+	<p class="intro">As you can see, this page is very experimental</p>
+</div>
+
--- src/routes/testpage2.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Test Page 2</h1>
-<a href="./">Back to Test Page 1</a>
